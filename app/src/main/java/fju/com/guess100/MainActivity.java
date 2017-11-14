e28097696f126e89be789c198bd3f4eea2c1261d
sungchian <bnbn870829@gmail.com>
2017-11-14 10:12:36 +0800

    findViews

--- app/src/main/java/fju/com/guess100/MainActivity.java
+++ app/src/main/java/fju/com/guess100/MainActivity.java
@@ -2,6 +2,10 @@ package fju.com.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    private void findViews(){
+        Button send = (Button) findViewById(R.id.send);
+        EditText number = (EditText) findViewById(R.id.number);
+        TextView secret = (TextView) findViewById(R.id.secret);
+        TextView info = (TextView) findViewById(R.id.info);
     }
 }
